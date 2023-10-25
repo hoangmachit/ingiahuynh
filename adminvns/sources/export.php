efedc3102f1f2c3f57e61fa425bb493443f4433f
Hoang Mach It <hoangmach.website@gmail.com>
2023-10-26 00:54:23 +0700

    Export

--- adminvns/sources/export.php
+++ adminvns/sources/export.php
@@ -13,11 +13,12 @@ switch ($act) {
 	case "man":
 		$template = "export/man/items";
 		break;
-
 	case "exportExcel":
 		exportExcel();
 		break;
-
+	case "exportExcelOptions";
+		exportOptions();
+		break;
 	default:
 		$template = "404";
 }
@@ -65,7 +66,7 @@ function exportExcel()
 				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(15);
 			} else if (in_array($k, ['tenvi', 'tenkhongdauvi'])) {
 				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(25);
-			}else if (in_array($k, ['motavi', 'motanganvi', 'motangan2vi'])) {
+			} else if (in_array($k, ['motavi', 'motanganvi', 'motangan2vi'])) {
 				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(50);
 			} else if ($k == 'noidungvi') {
 				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(150);
@@ -134,6 +135,232 @@ function exportExcel()
 		$time = time();
 		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 		header('Content-Disposition: attachment;filename="products_' . $time . '_' . date('d_m_Y') . '.xlsx"');
+		header('Cache-Control: max-age=0');
+		$objWriter = PHPExcel_IOFactory::createWriter($PHPExcel, 'Excel2007');
+		$objWriter->save('php://output');
+		exit();
+	} else {
+		$func->transfer("Dữ liệu rỗng", "index.php?com=export&act=man&type=" . $type, false);
+	}
+}
+function exportOptions()
+{
+	global $d, $func, $type;
+	/* Setting */
+	$setting = $d->rawQueryOne("select * from #_setting limit 0,1");
+	if (isset($type)) {
+		/* PHPExcel */
+		require_once LIBRARIES . 'PHPExcel.php';
+		/* Khởi tọa đối tượng */
+		$PHPExcel = new PHPExcel();
+		/* Khởi tạo thông tin người tạo */
+		$PHPExcel->getProperties()->setCreator($setting['tenvi']);
+		$PHPExcel->getProperties()->setLastModifiedBy($setting['tenvi']);
+		$PHPExcel->getProperties()->setTitle("Office 2007 XLSX Test Document");
+		$PHPExcel->getProperties()->setSubject("Office 2007 XLSX Test Document");
+		$PHPExcel->getProperties()->setDescription("Document for Office 2007 XLSX, generated using PHP classes.");
+		$alphas = array();
+		for ($i = 'A'; $i !== 'ZY'; $i++) {
+			array_push($alphas, $i);
+		}
+		$array_file = array(
+			"id" => "ID",
+			"tenvi" => "Tên sản phẩm",
+			"tenkhongdauvi" => "Đường dẫn sản phẩm",
+		);
+		$arrayKT = [
+			"kich_thuoc" => "Kích thước",
+		];
+		$arrayKTCL = [
+			"kho_in" => "Khổ in",
+			"chat_lieu" => "Chất liệu",
+			"total_count_decal" => "Số con khả thi trên 1 khổ decal",
+			"price_nl_m2" => "Giá NL + ịn 1m",
+			"price_nl" => "Giá ng NL",
+			"can_mang" => "Cán màng",
+			"quy_cach" => "Quy cách",
+			"mat_in" => "Mặt in",
+			"so_luong" => "Số lượng",
+			"thoi_gian" => "Thời gian",
+		];
+		$newArray = array_merge($array_file, $arrayKT, $arrayKTCL);
+		$i = 0;
+		foreach ($newArray as $k => $v) {
+			if ($k == 'masp') {
+				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(15);
+			} else if ($k == 'chat_lieu') {
+				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(50);
+			} else if (in_array($k, ['tenvi', 'tenkhongdauvi'])) {
+				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(25);
+			} else if (in_array($k, ['can_mang', 'quy_cach', 'mat_in', 'so_luong', 'thoi_gian'])) {
+				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(50);
+			} else {
+				$PHPExcel->getActiveSheet()->getColumnDimension($alphas[$i])->setWidth(15);
+			}
+			$PHPExcel->setActiveSheetIndex(0)->setCellValue($alphas[$i] . '1', $v);
+			$PHPExcel->getActiveSheet()->getStyle($alphas[$i] . '1')->applyFromArray(array('font' => array('color' => array('rgb' => 'ffffff'), 'name' => 'Calibri', 'bold' => true, 'italic' => false, 'size' => 10), 'alignment' => array('horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_LEFT, 'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER, 'wrap' => true), 'fill' => array('type' => PHPExcel_Style_Fill::FILL_SOLID, 'color' => array('rgb' => '007BFF'))));
+			$i++;
+		}
+
+		$products = $d->rawQuery("select id, tenvi, tenkhongdauvi from #_product where type = ? order by stt,id desc", array($type));
+		$vitri = 2;
+		for ($i = 0; $i < count($products); $i++) {
+			$j = 0;
+			$jj = 0;
+			$productId = $products[$i]['id'];
+			foreach ($array_file as $k => $v) {
+				$datacell = $products[$i][$k];
+				$PHPExcel->setActiveSheetIndex(0)->setCellValue($alphas[$j] . $vitri, htmlspecialchars_decode($datacell));
+				$j++;
+			}
+			$kichThuoc = $d->rawQuery("select id,length,width from #_product_kich_thuocs where product_id= ?", array($productId));
+			if (!empty($kichThuoc)) {
+				for ($k_index = 0; $k_index < count($kichThuoc); $k_index++) {
+					$jj = $j;
+					$kichThuocId = $kichThuoc[$k_index]['id'];
+					$dsKichThuocChatLieu = $d->rawQuery("select id,total_count_decal,price_nl_m2,price_nl,cl_id,ki_id from #_product_kich_thuoc_chat_lieus where kt_id= ?", array($kichThuocId));
+					foreach ($arrayKT as $k => $v) {
+						$datacell = $kichThuoc[$k_index]['length'] . " ~ " . $kichThuoc[$k_index]['width'];
+						$PHPExcel->setActiveSheetIndex(0)->setCellValue($alphas[$jj] . $vitri, htmlspecialchars_decode($datacell));
+						$jj++;
+					}
+					if (!empty($dsKichThuocChatLieu)) {
+						for ($ktcl_index = 0; $ktcl_index < count($dsKichThuocChatLieu); $ktcl_index++) {
+							$jjj = $jj;
+							$kt_cl_id = $dsKichThuocChatLieu[$ktcl_index]['id'];
+							// get Chất Liệu
+							$chatLieuId = $dsKichThuocChatLieu[$ktcl_index]['cl_id'];
+							$detailCL = $d->rawQueryOne("select name from #_product_chat_lieus where id=?", array($chatLieuId));
+							// get Khổ in
+							$khoInId = $dsKichThuocChatLieu[$ktcl_index]['ki_id'];
+							$detailKI = $d->rawQueryOne("select * from #_product_kho_ins where id=?", array($khoInId));
+
+							// get danh sách mặt in
+							$dsMatIn = $d->rawQuery("SELECT s1.id AS id1, s1.mi_id AS mi_id, s2.name AS name, s2.percent AS percent
+							FROM table_product_kich_thuoc_chat_lieu_mat_ins AS s1 , table_product_mat_ins AS s2
+							WHERE s1.mi_id = s2.id AND s1.ktcl_id = ?", array($kt_cl_id));
+							$stringMatIn = '';
+							if (!empty($dsMatIn)) {
+								foreach ($dsMatIn as $key => $mi) {
+									$stringMatIn .= $mi['name'] . ', ';
+								}
+								$stringMatIn = rtrim($stringMatIn, ', ');
+							}
+							// get danh sách cán màng
+							$dsCanMang = $d->rawQuery("SELECT s1.id AS id1, s1.cm_id AS cm_id, s2.name AS name, s2.percent AS percent
+							FROM table_product_kich_thuoc_chat_lieu_can_mangs AS s1 , table_product_can_mangs AS s2
+							WHERE s1.cm_id = s2.id AND s1.ktcl_id = ?", array($kt_cl_id));
+							$stringCanMang = '';
+							if (!empty($dsCanMang)) {
+								foreach ($dsCanMang as $key => $cm) {
+									$stringCanMang .= $cm['name'] . ', ';
+								}
+								$stringCanMang = rtrim($stringCanMang, ', ');
+							}
+							// get danh sách quy cách
+							$dsQuyCach = $d->rawQuery("SELECT s1.id AS id1, s1.qc_id AS qc_id, s2.name AS name, s2.point AS point
+							FROM table_product_kich_thuoc_chat_lieu_quy_cachs AS s1 , table_product_quy_cachs AS s2
+							WHERE s1.qc_id = s2.id AND s1.ktcl_id = ?", array($kt_cl_id));
+
+							$stringQuyCach = '';
+							if (!empty($dsQuyCach)) {
+								foreach ($dsQuyCach as $key => $qc) {
+									$stringQuyCach .= $qc['name'] . ', ';
+								}
+								$stringQuyCach = rtrim($stringQuyCach, ', ');
+							}
+
+
+							// get danh sách số lượng
+							$dsSoLuong = $d->rawQuery("SELECT s1.id AS id1, s1.sl_id AS sl_id, s2.name AS name, s2.count AS count
+							FROM table_product_kich_thuoc_chat_lieu_soluongs AS s1 , table_product_so_luongs AS s2
+							WHERE s1.sl_id = s2.id AND s1.ktcl_id = ?", array($kt_cl_id));
+
+							$stringSoLuong = '';
+							if (!empty($dsSoLuong)) {
+								foreach ($dsSoLuong as $key => $sl) {
+									$stringSoLuong .= $sl['name'] . ', ';
+								}
+								$stringSoLuong = rtrim($stringSoLuong, ', ');
+							}
+
+							// get danh sách thời gian
+							$dsThoiGian = $d->rawQuery("SELECT s1.id AS id1, s1.tg_id AS tg_id, s2.name AS name, s2.percent AS percent
+							FROM table_product_kich_thuoc_chat_lieu_thoi_gians AS s1 , table_product_thoi_gians AS s2
+							WHERE s1.tg_id = s2.id AND s1.ktcl_id = ?", array($kt_cl_id));
+
+							$stringThoiGian = '';
+							if (!empty($dsThoiGian)) {
+								foreach ($dsThoiGian as $key => $tg) {
+									$stringThoiGian .= $tg['name'] . ', ';
+								}
+								$stringThoiGian = rtrim($stringThoiGian, ', ');
+							}
+
+							foreach ($arrayKTCL as $k3 => $v3) {
+								if ($k3 == 'kho_in') {
+									$datacell = $detailKI['left'] . "x" . $detailKI['right'];
+								} else if ($k3 == 'chat_lieu') {
+									$datacell = $detailCL['name'];
+								} else if ($k3 == 'mat_in') {
+									$datacell = $stringMatIn;
+								} else if ($k3 == 'quy_cach') {
+									$datacell = $stringQuyCach;
+								} else if ($k3 == 'can_mang') {
+									$datacell = $stringCanMang;
+								} else if ($k3 == 'so_luong') {
+									$datacell = $stringSoLuong;
+								} else if ($k3 == 'thoi_gian') {
+									$datacell = $stringThoiGian;
+								} else {
+									$datacell = $dsKichThuocChatLieu[$ktcl_index][$k3];
+								}
+								$PHPExcel->setActiveSheetIndex(0)->setCellValue($alphas[$jjj] . $vitri, htmlspecialchars_decode($datacell));
+								$jjj++;
+							}
+							$vitri++;
+						}
+					} else {
+						$vitri++;
+					}
+				}
+			} else {
+				$vitri++;
+			}
+		}
+		/* Style cho các row dữ liệu */
+		$vitri = 2;
+		for ($i = 0; $i < count($products); $i++) {
+			$j = 0;
+			foreach ($array_file as $k => $v) {
+				$PHPExcel->getActiveSheet()->getStyle($alphas[$j] . $vitri)->applyFromArray(
+					array(
+						'font' => array(
+							'color' => array('rgb' => '000000'),
+							'name' => 'Calibri',
+							'bold' => false,
+							'italic' => false,
+							'size' => 10
+						),
+						'alignment' => array(
+							'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_LEFT,
+							'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+							'wrap' => true
+						)
+					)
+				);
+				$j++;
+			}
+			$vitri++;
+		}
+		/* Rename title */
+		$PHPExcel->getActiveSheet()->setTitle('Products List');
+		/* Khởi tạo chỉ mục ở đầu sheet */
+		$PHPExcel->setActiveSheetIndex(0);
+		/* Xuất file */
+		$time = time();
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header('Content-Disposition: attachment;filename="products_options_' . $time . '_' . date('d_m_Y') . '.xlsx"');
 		header('Cache-Control: max-age=0');
 		$objWriter = PHPExcel_IOFactory::createWriter($PHPExcel, 'Excel2007');
 		$objWriter->save('php://output');
--- adminvns/templates/export/man/items_tpl.php
+++ adminvns/templates/export/man/items_tpl.php
@@ -1,5 +1,6 @@
 <?php
-	$linkExportExcel = "index.php?com=export&act=exportExcel&type=".$type;
+$linkExportExcel = "index.php?com=export&act=exportExcel&type=" . $type;
+$linkExportExcelOptions = "index.php?com=export&act=exportExcelOptions&type=" . $type;
 ?>
 <!-- Content Header -->
 <section class="content-header text-sm">
@@ -12,63 +13,47 @@
         </div>
     </div>
 </section>
-<?php 
-    $arrayselect = array(
-        "size" => "Kích thước",
-        "chatlieu" => "Chất liệu giấy",
-        "somat" => "Số mặt in",
-        "canmang" => "Loại cán màng",
-        "khoanlo" => "Đóng số nhảy",
-        "soduongcung" => "Qui cách",
-        "soluong" => "Số lượng",
-        "hinhdang" => "Thời gian",
-        "cachthuc" => "Số lượng trang ruột",
-        "kieube" => "Số đường cấn",
-    );
-    
-    ?>
+<?php
+$arrayselect = array(
+    "size" => "Kích thước",
+    "chatlieu" => "Chất liệu giấy",
+    "somat" => "Số mặt in",
+    "canmang" => "Loại cán màng",
+    "khoanlo" => "Đóng số nhảy",
+    "soduongcung" => "Qui cách",
+    "soluong" => "Số lượng",
+    "hinhdang" => "Thời gian",
+    "cachthuc" => "Số lượng trang ruột",
+    "kieube" => "Số đường cấn",
+);
+
+?>
 <!-- Main content -->
 <section class="content">
-    <form method="post" action="<?=$linkExportExcel?>" enctype="multipart/form-data">
+    <form method="post" class="mb-3" action="<?= $linkExportExcel ?>" enctype="multipart/form-data">
         <div class="card card-primary card-outline text-sm mb-0">
             <div class="card-header">
                 <h3 class="card-title">Export danh sách dữ liệu</h3>
             </div>
             <div class="card-body">
-                <?php if(isset($config['export']['category']) && $config['export']['category'] == true) { ?>
-                    <div class="form-group-category row">
-                        <?php if(isset($config['product'][$type]['list']) && $config['product'][$type]['list'] == true) { ?>
-                            <div class="form-group col-md-3 col-sm-4">
-                                <label class="d-block" for="id_list">Danh mục cấp 1:</label>
-                                <?=$func->get_ajax_category('product', 'list', $type)?>
-                            </div>
-                        <?php } ?>
-                        <?php if(isset($config['product'][$type]['cat']) && $config['product'][$type]['cat'] == true) { ?>
-                            <div class="form-group col-md-3 col-sm-4">
-                                <label class="d-block" for="id_cat">Danh mục cấp 2:</label>
-                                <?=$func->get_ajax_category('product', 'cat', $type)?>
-                            </div>
-                        <?php } ?>
-                        <?php if(isset($config['product'][$type]['item']) && $config['product'][$type]['item'] == true) { ?>
-                            <div class="form-group col-md-3 col-sm-4">
-                                <label class="d-block" for="id_item">Danh mục cấp 3:</label>
-                                <?=$func->get_ajax_category('product', 'item', $type)?>
-                            </div>
-                        <?php } ?>
-                        <?php if(isset($config['product'][$type]['sub']) && $config['product'][$type]['sub'] == true) { ?>
-                            <div class="form-group col-md-3 col-sm-4">
-                                <label class="d-block" for="id_sub">Danh mục cấp 4:</label>
-                                <?=$func->get_ajax_category('product', 'sub', $type)?>
-                            </div>
-                        <?php } ?>
-                    </div>
-                <?php } else { ?>
-                    <div class="alert my-alert alert-info mb-0" role="alert">Xuất danh sách sản phẩm thành tập tin excel</div>
-                <?php } ?>
+                <div class="alert my-alert alert-info mb-0" role="alert">Xuất danh sách sản phẩm thành tập tin excel</div>
             </div>
         </div>
         <div class="card-footer text-sm">
-            <button type="submit" class="btn btn-sm bg-gradient-success" name="exportExcel"><i class="fas fa-file-export mr-2"></i>Export</button>
+            <button type="submit" class="btn btn-sm bg-gradient-success" name="exportExcel"><i class="fas fa-file-export mr-2"></i>Export thông tin cơ bản</button>
+        </div>
+    </form>
+    <form method="post" action="<?= $linkExportExcelOptions ?>" enctype="multipart/form-data">
+        <div class="card card-primary card-outline text-sm mb-0">
+            <div class="card-header">
+                <h3 class="card-title">Export danh sách dữ liệu có options</h3>
+            </div>
+            <div class="card-body">
+                <div class="alert my-alert alert-info mb-0" role="alert">Xuất danh sách sản phẩm thành tập tin excel</div>
+            </div>
+        </div>
+        <div class="card-footer text-sm">
+            <button type="submit" class="btn btn-sm bg-gradient-danger" name="exportExcel"><i class="fas fa-file-export mr-2"></i>Export với các options</button>
         </div>
     </form>
 </section>
